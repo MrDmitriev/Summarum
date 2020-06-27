cd0628c1adb6cf2b3675e3ad7625b977ca6681b5
dmitriev_sergey <info@davra.cz>
2020-06-27 11:42:38 +0200

    Added base pages

---
+++ src/pages/CreateSummaryPage/CreateSummaryPage.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export class CreateSummaryPage extends Component {
+  render() {
+    return (
+      <div>Hello Youpta</div>
+    );
+  }
+}
+
+export default CreateSummaryPage;
---
+++ src/pages/DashboardPage/DashboardPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import SummariesList from '../../components/organisms/SummariesList/SummariesList';
+
+export class DashboardPage extends Component {
+  render() {
+    return (
+      <SummariesList />
+    );
+  }
+}
+
+export default DashboardPage;
+
